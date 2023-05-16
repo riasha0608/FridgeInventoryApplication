f88fe1c7b30580458d95c2a9989e8a1ac5c70003
riasha24 <riajshah@gmail.com>
2023-05-16 14:51:49 -0400

    initial basic prototype (to be changed)

---
+++ calendar.js
@@ -0,0 +1,17 @@
+
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+
+function myFunction2() {
+  var popup = document.getElementById("myPopup2");
+  popup.classList.toggle("show");
+}
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
